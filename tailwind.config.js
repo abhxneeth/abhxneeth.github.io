6f1c4a2c57df07cc5299bf362520ffc6c2fd3369
abhineeth <110736764+abhxneeth@users.noreply.github.com>
2022-09-05 23:41:01 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        fontFamily: {
+          inter: "'Inter', sans-serif",
+        }
+      },
+    },
   },
   plugins: [],
 }
